b5296a91003579c629fdd391b3c013dcfc988e79
Sidharth Ajay Nagath <sidnagath@gmail.com>
2026-05-23 14:23:39 +0530

    Implemented lesson management and video uploads

---
+++ models/lessonModel.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const lessonSchema = new mongoose.Schema(
+
+  {
+
+    // MODULE REFERENCE
+
+    moduleId: {
+
+      type: mongoose.Schema.Types.ObjectId,
+
+      ref: "Module",
+
+      required: true
+
+    },
+
+
+    // LESSON TITLE
+    title: {
+      type: String,
+      trim: true
+    },
+
+    // LESSON DESCRIPTION
+    description: {
+      type: String,
+      trim: true
+    },
+
+
+
+    // VIDEO FILE
+    video: {
+      type: String
+    },
+
+    // VIDEO DURATION
+    duration: {
+      type: String
+    },
+
+    // LESSON ORDER
+    order: {
+      type: Number,
+      default: 1
+    },
+
+    // FREE PREVIEW
+    isPreviewFree: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  // AUTO CREATED AT / UPDATED AT
+
+  {
+    timestamps: true
+  }
+);
+
+
+
+module.exports = mongoose.model(
+  "Lesson",
+  lessonSchema
+);
